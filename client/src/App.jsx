419d57d8cd540b65cbe5a0604bc2e4430168ac5d
ashhwanigupta <Ashhwanigupta1@gmail.com>
2023-10-30 04:04:40 +0530

    sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
 import Header from './components/Header';
+import SignUp from './pages/SignUp';
 
 function App() {
 
@@ -17,7 +17,7 @@ function App() {
       <Route path="/about" element={<About/>} />
       <Route path="/profile" element={<Profile/>} />
       <Route path="/sign-in" element={<SignIn/>} />
-      <Route path="/sign-out" element={<SignOut/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
     </Routes>
   </Router>
   )
